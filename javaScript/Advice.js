d9f2daaf915598c2b4ae7a573fe70b58c20237f7
Safi Nafi <79153374+Safi100@users.noreply.github.com>
2022-03-08 17:20:06 +0200

    ...

--- javaScript/Advice.js
+++ javaScript/Advice.js
@@ -5,11 +5,9 @@ dice_button.addEventListener('click', RotateDice)
 
 function RotateDice(){
     let advice_number = Math.floor(Math.random() * 224) + 1
-    console.log(advice_number)
     fetch(`https://api.adviceslip.com/advice/${advice_number}`)
     .then(res => res.json())
     .then(data =>{
-         console.log(data.slip)
          document.querySelector('.advice_number').innerHTML = `Advice # ${data.slip.id}`
          document.querySelector('.quotes').innerHTML = `${data.slip.advice}`
     })
